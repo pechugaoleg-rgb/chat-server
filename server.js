8c1ee196133d2a67cd4cde43296a503bbbfcc4b0
pechugaoleg-rgb <pechugaoleg@gmail.com>
2026-09-10 20:14:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,27 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
 
-io.on('connection', (socket) => {
-    console.log('Користувач підключився');
-
-    socket.on('chat_message', (msg) => {
-        io.emit('chat_message', msg); // Розсилка всім
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Користувач відключився');
-    });
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Дозволяє підключатися з будь-якого джерела
+    methods: ["GET", "POST"]
+  }
 });
 
-server.listen(3000, () => {
-    console.log('Сервер чату запущено на порту 3000');
+io.on('connection', (socket) => {
+  console.log('Користувач підключився');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Користувач відключився');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Сервер чату запущено на порту ${PORT}`);
 });
